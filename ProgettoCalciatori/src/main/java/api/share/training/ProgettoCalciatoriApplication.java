24a4d4d7dfba89df10fb022e6b554bc9fccafec1
DanielePorzia <daniele.porzia@usergroup.it>
2022-10-13 09:33:00 +0200

    Commit in cui creo i packages

---
+++ ProgettoCalciatori/src/main/java/api/share/training/ProgettoCalciatoriApplication.java
@@ -0,0 +1,13 @@
+package api.share.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoCalciatoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoCalciatoriApplication.class, args);
+	}
+
+}
---
+++ ProgettoCalciatori/src/test/java/api/share/training/ProgettoCalciatoriApplicationTests.java
@@ -0,0 +1,13 @@
+package api.share.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoCalciatoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
